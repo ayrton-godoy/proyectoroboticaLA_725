2acd0ea2a95ad0db4a85923de0929bfca984af4e
LucianoPino2007 <115808799+LucianoPino2007@users.noreply.github.com>
2024-10-25 10:02:13 -0300

    cambios

---
+++ script.js
@@ -0,0 +1,30 @@
+function handleConnect() {
+    const statusMessage = document.getElementById('statusMessage');
+    
+    const devicesConnected = false; 
+    if (!devicesConnected) {
+        alert("No hay dispositivos conectados."); // Ventana emergente
+    } else {
+        const statusMessage = document.getElementById('statusMessage');
+        statusMessage.innerText = "Conectando...";
+    }
+
+    if (!devicesConnected) {
+        statusMessage.innerText = "No hay dispositivos conectados.";
+    } else {
+        statusMessage.innerText = "Conectando...";
+        
+        setTimeout(() => {
+            statusMessage.innerText = "Conexión exitosa.";
+        }, 2000);
+    }
+}
+
+function handleAnalyze() {
+    const statusMessage = document.getElementById('statusMessage');
+    statusMessage.innerText = "Analizando...";
+
+    setTimeout(() => {
+        statusMessage.innerText = "Análisis rechazado, no hay dispositivo conectado.";
+    }, 2000);
+}
